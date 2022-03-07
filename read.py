0e0edb25ebe4417e08310a079671707b3f8dbe74
Xinli Xiao <xiaoxinlixxl@gmail.com>
2022-03-06 23:59:26 -0600

    3/6/22

---
+++ read.py
@@ -0,0 +1,16 @@
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+
+# %%
+import matplotlib.pyplot as plt
+
+plt.imshow(x_test[0])
+# %%
+
+# %%
+
+# %%
